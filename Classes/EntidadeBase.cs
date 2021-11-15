54a838c13d0295d03704eba112f145a034192282
Débora Silva <debora.raquel710@gmail.com>
2021-11-15 19:08:59 -0300

    criação do crud

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,8 @@
+namespace DIO.Dramas
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+        
+    }
+}
---
+++ Enum/Genero.cs
@@ -0,0 +1,20 @@
+namespace DIO.Dramas
+{
+    public enum Genero
+    {
+        Acao = 1,
+        Aventura = 2,
+        Comedia = 3, 
+        Crime = 4,
+        Drama = 5,
+        Fantasia = 6,
+        Ficcao_Cientifica = 7,
+        Romance = 8,
+        Suspense = 9,
+        Terror = 10,
+        Psicologico = 11,
+        Historico = 12,
+        Slice_of_life = 13 
+
+    }
+}
